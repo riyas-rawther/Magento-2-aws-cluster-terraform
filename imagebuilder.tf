d507c6879d7bb50b09e3175790ce0c4b3285145a
Riyas Rawther <rrauther@primahealthcredit.com>
2023-09-13 14:42:48 +0530

    reduce builder spec

--- imagebuilder.tf
+++ imagebuilder.tf
@@ -106,7 +106,7 @@ resource "aws_imagebuilder_infrastructure_configuration" "this" {
   name                  = "${local.project}-${each.key}-imagebuilder-infrastructure"
   description           = "ImageBuilder infrastructure for ${each.key} in ${local.project}"
   instance_profile_name = aws_iam_instance_profile.ec2[each.key].name
-  instance_types        = ["c6g.xlarge"]
+  instance_types        = ["c6g.medium"]
   security_group_ids    = [aws_security_group.ec2.id,aws_security_group.efs.id]
   sns_topic_arn         = aws_sns_topic.default.arn
   subnet_id             = values(aws_subnet.this).0.id
